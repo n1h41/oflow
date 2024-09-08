a45c79e1dca402667e14ae129bf6cf8c9a6cac80
n1h41 <nihalninu25@gmail.com>
2024-09-08 21:18:41 +0530

    create route for user signup and register signup handler it. Implemented userSignup method in user repo

--- internal/model/user_model.go
+++ internal/model/user_model.go
@@ -1,9 +1,9 @@
 package model
 
-type CreatUserModel struct {
-	Email     string `json:"email"`
+type CreateUserModelReq struct {
+	Email     string `json:"email" validate:"required"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Phone     string `json:"phone"`
-	Password  string `json:"password"`
+	Password  string `json:"password" validate:"required"`
 }
